4ab341930ed66e360b2d44184dbb323f8c1d7cca
David Pelaez <pelaez89@gmail.com>
2011-10-21 10:05:03 -0500

    Rewriting navtree from scratch. Data loading is working

---
+++ eclipse/sketch2_edges/src/navtree/Edge.java
@@ -0,0 +1,26 @@
+package navtree;
+
+public class Edge {
+
+	public Node from, to;
+	public float length;
+	public int fromId, toId;
+	private Navtree navtree;
+
+	Edge(Navtree _navtree, int _fromId, int _toId) {
+		navtree = _navtree;
+		fromId = _fromId;
+		toId = _toId;
+	}
+
+	public void pointToNodes() {
+		from = navtree.findNode(fromId);
+		to = navtree.findNode(toId);
+
+	}
+
+	public void draw() {
+
+	}
+
+}
